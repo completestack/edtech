00ce8866a6a17e4b0ac9c3e35d0635d024f66e78
Praveen <74362526+completestack@users.noreply.github.com>
2023-01-17 15:22:22 +0530

    test svelte project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+	import Counter from './Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		<span class="welcome">
+			<h1>Welcome</h1>
+		</span>
+
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/+page.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
